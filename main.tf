ddd2009f478541702079847f2106037d12564918
KhaNguyennn <kha.nguyen@nexlab.tech>
2024-07-13 11:55:01 +0700

    update deploy nginx

--- main.tf
+++ main.tf
@@ -67,6 +67,34 @@ resource "aws_security_group" "instance" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 
+  ingress {
+    from_port = 139
+    to_port = 139
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+    from_port = 445
+    to_port = 445
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  
   egress {
     from_port = 0
     to_port = 0
