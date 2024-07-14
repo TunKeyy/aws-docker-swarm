1d8dba61533d341b3818171ff0bba570a366bfe1
KhaNguyennn <kha.nguyen@nexlab.tech>
2024-07-14 11:16:48 +0700

    update cd

---
+++ deploy/deploy.sh
@@ -0,0 +1,17 @@
+SWARM_IP=$1
+SWARM_USER=$2
+SSH_PRIVATE_KEY=$3
+DEPLOY_FILE=docker-compose.yml
+STACK_NAME=t1
+# loading section
+echo "/-\|/-\|/-\|/-\|/-\|/-\|/-\|"
+# end
+
+mkdir -p ~/.ssh/id_rsa
+echo "$SSH_PRIVATE_KEY" >> ~/.ssh/id_rsa
+
+# Copy Docker configuration to Node manager
+scp -i ~/.ssh/id_rsa ./deploy/$DEPLOY_FILE $SWARM_USER@$SWARM_IP:/home/ubuntu
+
+ssh -tt -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $SWARM_USER@$SWARM_IP "sudo docker stack deploy -c $DEPLOY_FILE $STACK_NAME"
+echo "Deploy successfully"
