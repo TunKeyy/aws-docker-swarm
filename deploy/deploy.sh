2f2f696645f6363f06e084f920fad95bcec1372b
KhaNguyennn <kha.nguyen@nexlab.tech>
2024-07-14 23:44:35 +0700

    new update

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,5 +15,7 @@ chmod 600 ~/.ssh/id_rsa
 # Copy Docker configuration to Node manager
 scp -i ~/.ssh/id_rsa ./deploy/$DEPLOY_FILE $SWARM_USER@$SWARM_IP:/home/ubuntu
 
+ssh -tt -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $SWARM_USER@$SWARM_IP "ls -l /home/ubuntu/$DEPLOY_FILE"
+
 ssh -tt -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $SWARM_USER@$SWARM_IP "sudo docker stack deploy -c /home/ubuntu/$DEPLOY_FILE $STACK_NAME"
 echo "Deploy successfully"
